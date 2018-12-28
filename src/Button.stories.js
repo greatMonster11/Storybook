95c3dd5c54aa5d79bfc12fb88e5ded3ed5632014
greatMonster11 <nguyenphuocthanhtn2015@gmai.com>
2018-12-28 10:18:29 +0700

    add addon-knobs

--- src/Button.stories.js
+++ src/Button.stories.js
@@ -1,30 +1,25 @@
 import React from 'react'
-import { withInfo } from '@storybook/addon-info'
+//import { withInfo } from '@storybook/addon-info'
 
+import { wInfo } from './utils'
 import { storiesOf } from '@storybook/react'
 import { Button } from './Button'
+import { color } from '@storybook/addon-knobs'
+
 
 storiesOf('Button', module)
   .addWithJSX(
     'with background',
-    withInfo({
-      styles: {
-        header: {
-          h1: {
-            color: 'red'
-          }
-        }
-      },
-      text: `
+    wInfo(`
         despcription
 
         ~~~js
           <Button>Click here</Button>
         ~~~
-      `
-    })(() => (
-      <Button bg={"tomato"}>Hello world</Button>
-    )))
+      `)
+      (() => (
+        <Button bg={"tomato"}>Hello world</Button>
+      )))
   .addWithJSX('with background 2', () => (
-    <Button bg={"mediumseagreen"}>Hello world</Button>
+    <Button bg={color('bg', 'green', 'group1')}>Hello world</Button>
   ))
